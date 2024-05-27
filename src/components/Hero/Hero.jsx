a5ef37ca9e70bdde12e7009a67b16c537d7b70fa
SETYA HADI NUGROHO <222112358@stis.ac.id>
2024-05-27 13:00:16 +0700

    respon

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = ({ handleOrderPopup }) => {
   };
 
   return (
-    <div className="relative lg:translate-x-0 translate-x-24 container overflow-hidden w-[430px] lg:w-screen lg:h-[400px] h-[700px] bg-white lg:border-4 border-8 mt-10 dark:mt-0 rounded-xl flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200 drop-shadow-[-5px_5px_8px_rgba(0,0,0,0.3)] ">
+    <div className="relative lg:translate-x-0 translate-x-20 container overflow-hidden w-[430px] lg:w-screen lg:h-[400px] h-[700px] bg-white lg:border-4 border-8 mt-10 dark:mt-0 rounded-xl flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200 drop-shadow-[-5px_5px_8px_rgba(0,0,0,0.3)] ">
       {/* background pattern */}
       <div className="h-[600px] w-[600px] bg-gray-200 dark:bg-gray-700 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z[8]"></div>
       {/* hero section */}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -137,7 +137,7 @@ const Navbar = () => {
     window.scrollTo({ top: document.documentElement.scrollHeight, behavior: "smooth" });
   };
   return (
-    <div className="shadow-md bg-white dark:bg-gray-900 w-[550px] lg:w-screen dark:text-white duration-200 relative z-40">
+    <div className="shadow-md bg-white dark:bg-gray-900 w-[500px] lg:w-screen dark:text-white duration-200 relative z-40">
       {/* Upper Navbar */}
       <div className="lg:px-10 bg-green-500 py-2 sm:h-[150px] lg:h-[130px]">
         <div className="container mt-3 sm: flex justify-between items-center">
