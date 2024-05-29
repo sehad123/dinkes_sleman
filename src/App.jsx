a8b63a26b2fefa2f53b57e85b0c8d84125f504f0
SETYA HADI NUGROHO <222112358@stis.ac.id>
2024-05-29 17:23:29 +0700

    p

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Berita />} />
+            <Route index element={<PerijinanKesehatan />} />
             <Route path="agenda" element={<Agenda />} />
             <Route path="berita" element={<Berita />} />
             {/* profil */}
